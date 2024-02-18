8266b61ef1295d1a498820e6b747963de248e851
c0deb1ind31 <rhutikparab1@gmail.com>
2024-02-19 04:48:40 +0530

    feat: added execution time calculation

--- backend/submissions/tasks.py
+++ backend/submissions/tasks.py
@@ -4,13 +4,14 @@ from problems.models import Problems
 
 from celery import shared_task
 import subprocess
-
+import time
 
 @shared_task
 def run_submission(submission_id, code, inputs, code_lang):
+    start_time = time.time()
+
     submission=Submissions.objects.get(id=submission_id)
-    problem=Problems.objects.get(id=submission_id.problem_id)
-    print(problem)
+    problem=Problems.objects.get(id=submission.problem_id)
     try:
         if code_lang == 'python':
             op = subprocess.run(["python", "-c", code],capture_output=True, text=True, check=True)
@@ -33,6 +34,8 @@ def run_submission(submission_id, code, inputs, code_lang):
         submission.result="Runtime Error"
         submission.output= e.stderr
 
+    end_time = time.time()
+    submission.execution_time_ms= (end_time - start_time)*1000
 
     submission.status="completed"
     submission.save()
