122f35a84568ce1d8b7a91acb0a49fdb88ccab17
c0deb1ind31 <rhutikparab1@gmail.com>
2024-02-19 04:17:49 +0530

    feat : added view for submissions

--- backend/submissions/views.py
+++ backend/submissions/views.py
@@ -1,24 +1,50 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
-from .tasks  import run_submission
+from .models import Submissions
+from .serializer import SubmissionsSerialiser
 
+from .tasks import run_submission
 # Create your views here.
 @api_view(['GET'])
 def getAllSubmissions(request,pid):
-    run_submission.delay()
-    return Response("gets all submissions")
+    submission = Submissions.objects.all()
+    serialiser = SubmissionsSerialiser(submission)
+    return Response(data=serialiser.data,status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def makeSubmission(request, pid):
+    data = request.data
+    submission = Submissions(user_id=data["user_id"], problem_id=pid, code_lang=data["lang"], submission_code=data["code"],
+                            submission_type=data["submission_type"], test_input=data["test_input"], status="pending")
+    submission.save()
+    run_submission.delay(submission.id,submission.submission_code,submission.test_input,submission.code_lang)
+    data={'submission_id':submission.id}
+
+    return Response(data)
+
 
 @api_view(['GET'])
-def getUserSubmissions(request,pid):
-    return Response("Gets all user submissions")
+def getUserSubmissions(request,uid):
+    submission = Submissions.objects.filter(user_id=uid)
+    serialiser = SubmissionsSerialiser(submission)
+    return Response(data=serialiser.data,status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
 def getSubmission(request,sub_id):
-    return Response("gets submissions  from id")
+    submission = Submissions.objects.get(id=sub_id)
+    serialiser = SubmissionsSerialiser(submission)
+    return Response(data=serialiser.data,status=status.HTTP_200_OK)
 
 @api_view(['GET'])
-def checkSubmissionStatus(request,pid,sub_id):
-    return Response("check submissions status")
+def checkSubmissionStatus(request,sub_id):
+    submission = Submissions.objects.get(id=sub_id)
+    serialiser = SubmissionsSerialiser(submission)
+    if serialiser.data['status']=="pending":
+        data = {'status':'pending'}
+        return Response(data=data,status=status.HTTP_200_OK)
+
+    return Response(data=serialiser.data,status=status.HTTP_200_OK)
