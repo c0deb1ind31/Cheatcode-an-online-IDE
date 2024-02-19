58ad459eca1b4363275fa87065af4a10cec4205e
c0deb1ind31 <rhutikparab1@gmail.com>
2024-02-20 03:52:39 +0530

    refact: changed check submission url

--- backend/submissions/urls.py
+++ backend/submissions/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('submit/<int:pid>', views.makeSubmission),
     path('all_submissions/<int:pid>', views.getAllSubmissions),
-    path('user_submissions/<str:uid>', views.getUserSubmissions),
+    path('user_submissions/<str:pid>/user/<str:uid>', views.getUserSubmissions),
     path('<str:sub_id>', views.getSubmission),
     path('<str:sub_id>/check', views.checkSubmissionStatus),
 ]
