f81408b5fc5227544eff1305d4beb12bc1983308
c0deb1ind31 <rhutikparab1@gmail.com>
2024-02-19 17:58:09 +0530

    feat: added theme provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+
+import "./components/IDECodeEditor";
+import IDECodeEditor from "./components/IDECodeEditor";
+import QuestionView from "./components/QuestionView/QuestionView";
+import Console from "./components/Console";
+import { ThemeProvider } from "./context/themeprovider";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider defaultTheme='dark' storageKey="vite-ui-theme">
+      <div className="flex bg-blue w-screen h-screen bg-[#1A1A1A] gap-4 p-5">
+        <QuestionView />
+        <div className="flex flex-col flex-1 h-full gap-4">
+          <IDECodeEditor />
+          <Console />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
