0c374f07778432794e9b929f9673a4ea72dcf8dd
c0deb1ind31 <rhutikparab1@gmail.com>
2024-02-19 17:59:17 +0530

    feat: added code editor component

---
+++ frontend/src/components/IDECodeEditor.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+export default function IDECodeEditor() {
+  const default_code = {
+    python: `class solution:\n    def code(inputs,output):\n        //type your code here\n        return answer `,
+    javascript: `function main(nums, target) {\n  return a + b;\n}`,
+  };
+  const [code, setCode] = useState(default_code.python);
+  const [codeLang, setCodeLang] = useState<"python" | "javascript">("python");
+  useEffect(() => {
+    setCode(default_code[codeLang]);
+  }, [codeLang]);
+
+  function handleReset() {
+    setCode(default_code[codeLang]);
+  }
+  return (
+    <div className="flex-2 flex flex-col rounded-md bg-[#282828] w-tc-editor-var overflow-hidden p-2">
+      <div className="flex justify-between">
+        <Select
+          defaultValue="python"
+          onValueChange={(val: "python" | "javascript") => setCodeLang(val)}
+        >
+          <SelectTrigger className="w-[180px] dark:bg-[#333333]">
+            <SelectValue placeholder="Select a Language" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Language</SelectLabel>
+              <SelectItem value="python">Python</SelectItem>
+              <SelectItem value="javascript">Javascript</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button
+          className="dark:bg-[#424242] dark:hover:bg-[#42424]"
+          variant={"outline"}
+          onClick={handleReset}
+        >
+          Reset
+        </Button>
+      </div>
+      <div className="flex-1 max-h-[300px] overflow-scroll">
+        <CodeEditor
+          minHeight={300}
+          value={code}
+          language={codeLang}
+          data-color-mode="dark"
+          placeholder="Please enter JS code."
+          onChange={(evn) => setCode(evn.target.value)}
+          style={{
+            backgroundColor: "#282828",
+            fontSize: 16,
+            fontFamily:
+              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+          }}
+        />
+      </div>
+      <div className="self-end flex gap-2">
+        <Button
+          variant="outline"
+          className="dark:bg-zinc-600 dark:hover:bg-zinc-700 "
+        >
+          Run
+        </Button>
+        <Button
+          variant="outline"
+          className="dark:bg-green-600 dark:hover:bg-green-700"
+        >
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+}
