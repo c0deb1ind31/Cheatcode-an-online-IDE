2e50defe05ff132ee4a118c8293e3ea093a5a93c
c0deb1ind31 <rhutikparab1@gmail.com>
2024-02-20 03:51:35 +0530

    fix: filter model based on ids

--- backend/submissions/views.py
+++ backend/submissions/views.py
@@ -12,7 +12,8 @@ from .tasks import run_submission
 
 @api_view(['GET'])
 def getAllSubmissions(request, pid):
-    submission = Submissions.objects.filter(problem_id=uid)
+    submission = Submissions.objects.filter(
+        problem_id=pid, submission_type="submit", status="completed").order_by("-submitted_at")
     serialiser = SubmissionsSerialiser(submission, many=True)
     return Response(data=serialiser.data, status=status.HTTP_200_OK)
 
@@ -21,7 +22,7 @@ def getAllSubmissions(request, pid):
 def makeSubmission(request, pid):
     data = request.data
     submission = Submissions(user_id=data["user_id"], problem_id=pid, code_lang=data["lang"], submission_code=data["code"],
-                             submission_type=data["submission_type"], inputs=data["inputs"], status="pending")
+                             submission_type=data["submission_type"], status="pending")
     submission.save()
     run_submission.delay(submission.id, submission.submission_code,
                          submission.inputs, submission.code_lang)
@@ -31,8 +32,8 @@ def makeSubmission(request, pid):
 
 
 @api_view(['GET'])
-def getUserSubmissions(request, uid):
-    submission = Submissions.objects.filter(user_id=uid)
+def getUserSubmissions(request, pid,uid):
+    submission = Submissions.objects.filter(problem_id=pid, user_id=uid).order_by("-submitted_at")
     serialiser = SubmissionsSerialiser(submission, many=True)
     return Response(data=serialiser.data, status=status.HTTP_200_OK)
 
