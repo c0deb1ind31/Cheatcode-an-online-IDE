dd8a16f033ef31d93d51e310464f06e17e4e5595
c0deb1ind31 <rhutikparab1@gmail.com>
2024-02-20 06:08:41 +0530

    fix: cors fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-$s5rlwo)z@cbc2k&3_7w-=*!s9+s8*f7*c-$_%6!zs61p_7yk!
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost",
-                 'ec2-16-171-195-145.eu-north-1.compute.amazonaws.com', "http://ec2-16-171-195-145.eu-north-1.compute.amazonaws.com"]
+                 "http://16.171.195.145",
+                 "16.171.195.145",
+                 "ec2-16-171-195-145.eu-north-1.compute.amazonaws.com", "http://ec2-16-171-195-145.eu-north-1.compute.amazonaws.com"]
 
 # Application definition
 
@@ -63,7 +65,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
-    'ec2-16-171-195-145.eu-north-1.compute.amazonaws.com', "http://ec2-16-171-195-145.eu-north-1.compute.amazonaws.com"]
+    "http://16.171.195.145",
+    "16.171.195.145",
+    "ec2-16-171-195-145.eu-north-1.compute.amazonaws.com", "http://ec2-16-171-195-145.eu-north-1.compute.amazonaws.com"]
 
 
 CORS_ALLOW_METHODS = [
