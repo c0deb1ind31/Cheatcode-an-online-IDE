d68e00d7e5c0a2d44b3399b898d42b4aec74a17c
c0deb1ind31 <rhutikparab1@gmail.com>
2024-03-01 13:33:54 +0530

    refact: added google uid field in model

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Users(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    gid=models.TextField(unique=True, null=True)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
     reputation = models.IntegerField(default=0)
